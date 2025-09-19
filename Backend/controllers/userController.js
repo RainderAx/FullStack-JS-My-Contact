37711c306039a8433ae9e5315d3bff6910fdd1c1
RainderAx <sbst1.elz@gmail.com>
2025-09-19 12:04:45 +0200

    login register

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 mongoose.connect(process.env.MONGO_URI, {
     dbName: process.env.DB_NAME,
@@ -38,7 +39,12 @@ async function register(req, res) {
             email,
             password: hashedPassword,
         });
+
+        const token
+
         await newUser.save();
+
+
         res.status(201).json({ message: 'Utilisateur enregistré avec succès' });
     } catch (error) {
         console.error('Erreur lors de l\'enregistrement de l\'utilisateur :', error);
@@ -46,6 +52,37 @@ async function register(req, res) {
     }
 }
 
+const verifyPassword = async (password, hashedPassword) => {
+    try {
+        const match = await bcrypt.compare(password, hashedPassword);
+        if (match) {
+            console.log('Le mot de passe est correct');
+        } else {
+            console.log('Le mot de passe est incorrect');
+        }
+        return match;
+    } catch (error) {
+        console.error('Erreur lors de la vérification du mot de passe :', error);
+        throw error;
+    }
+};
+
+async function login(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+        return res.status(400).json({ message: 'Utilisateur non trouvé' });
+    }
+    const isPasswordValid = await verifyPassword(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Mot de passe incorrect' });
+    }
+    res.status(200).json({ message: 'Connexion réussie' });
+}
+
+async function tok()
+
 module.exports = {
-    register
+    register,
+    login
 };
--- Backend/views/userRoutes.js
+++ Backend/views/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { register } = require('../controllers/userController');
+const { login } = require('../controllers/userController');
 
 // Route d'inscription
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;

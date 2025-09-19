92ed6c5aac9a5f29ea1a66f4901be35d719b8859
RainderAx <sbst1.elz@gmail.com>
2025-09-19 13:55:36 +0200

    token ok

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -40,12 +40,10 @@ async function register(req, res) {
             password: hashedPassword,
         });
 
-        const token
-
         await newUser.save();
 
+        res.status(201).json({ message: 'Utilisateur enregistré avec succès', token });
 
-        res.status(201).json({ message: 'Utilisateur enregistré avec succès' });
     } catch (error) {
         console.error('Erreur lors de l\'enregistrement de l\'utilisateur :', error);
         res.status(500).json({ message: 'Erreur serveur' });
@@ -74,13 +72,18 @@ async function login(req, res) {
         return res.status(400).json({ message: 'Utilisateur non trouvé' });
     }
     const isPasswordValid = await verifyPassword(password, user.password);
+
     if (!isPasswordValid) {
         return res.status(400).json({ message: 'Mot de passe incorrect' });
     }
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    console.log('Token JWT généré :', token);
+
     res.status(200).json({ message: 'Connexion réussie' });
 }
 
-async function tok()
+
 
 module.exports = {
     register,
